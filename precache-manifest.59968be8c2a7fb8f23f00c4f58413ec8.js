8cc49d7e851a67619ffd31a23f85304726e4efe8
extrovert-kerwin <2038290448@qq.com>
2024-12-04 18:36:58 +0800

    Updates

---
+++ precache-manifest.59968be8c2a7fb8f23f00c4f58413ec8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5199f38865997ff1c57f",
+    "url": "/persimmon/static/css/main.8cb59933.chunk.css"
+  },
+  {
+    "revision": "5199f38865997ff1c57f",
+    "url": "/persimmon/static/js/main.5199f388.chunk.js"
+  },
+  {
+    "revision": "1280b9debcc31ff10066",
+    "url": "/persimmon/static/js/runtime~main.1280b9de.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/persimmon/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "89b5aae512c1e70d5568bddc5f3d06d0",
+    "url": "/persimmon/index.html"
+  }
+];
